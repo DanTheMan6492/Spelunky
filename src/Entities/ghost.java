01799bb9b0fb20cae6588f9fffd3e1703d1a64d9
auraw <auraw@10.9.134.131>
2022-05-26 12:27:31 -0700

    More enemy coding.

--- src/Entities/ghost.java
+++ src/Entities/ghost.java
@@ -1,5 +1,11 @@
 package Entities;
 
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.net.URL;
+
 public class ghost extends Entity{
 
 	public ghost(int x, int y, int w, int h, boolean visible, String path) {
@@ -7,4 +13,19 @@ public class ghost extends Entity{
 		// TODO Auto-generated constructor stub
 	}
 
+	public void paint(Graphics g) {
+		update();
+		Graphics2D g2 = (Graphics2D) g;
+		g2.drawImage(Sprite, (int) (x-Camera.x), (int) (y-Camera.y), dir * (int) w, (int) h, null);
+	}
+
+	protected Image getImage(String path) {
+
+		Image tempImage = null;
+		try {
+			URL imageURL = Entity.class.getResource(path);
+			tempImage    = Toolkit.getDefaultToolkit().getImage(imageURL);
+		} catch (Exception e) {e.printStackTrace();}
+		return tempImage;
+	}	
 }
