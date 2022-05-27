728749ec72a09dd1614a00ee793dbac0ae2b1412
auraw <auraw@10.9.134.131>
2022-05-27 10:52:43 -0700

    working on alien

--- src/Entities/alien.java
+++ src/Entities/alien.java
@@ -6,25 +6,111 @@ import java.awt.Image;
 import java.awt.Toolkit;
 import java.net.URL;
 
+import Blocks.Block;
+import Blocks.LevelBuilder;
+
 public class alien extends Entity
 {
+	public int moveTimer;
+	
 	public alien(int x, int y, int w, int h, boolean visible, String path) 
 	{
 		super(x, y, w, h, visible, path);
+		dir = 1;
+		vx = 0;
+		vy = 0;
+		grounded = true;
+		Sprite = getImage("/imgs/Characters/Spliced/snakeStandRight.gif");
 	}
 	
-	public void update() {
-		tx.setToTranslation(x, y);
+	
+	public void checkGround() {
+		int mapX = (int) (x / 128);
 		
-		if(vx < -0.5) 
-			dir = -1;
-		else if(vx > 0.5)
-			dir = 1;
+		if((mapX == 0 && vx < 0) || (mapX == 32 && vx > 0)) {
+			vx = 0;
+			dir *= -1;
+			return;
+		}
+		
+		if(mapX == 0) {
+			if(vx < 0) {
+				vx = 0;
+				dir *= -1;
+			}
+			return;
+		}
+		else if(mapX == 39) 
+		{
+			if(vx > 0) 
+			{
+				vx = 0;
+				dir *= -1;
+			}
+			return;
+		}
+
+		int XProj = (int) ((x+vx+64) / 128);
+		int YProj = (int) ((y+vy) / 128);
+
+		if(LevelBuilder.level[YProj+1][XProj] == null
+		|| LevelBuilder.level[YProj+1][XProj].solid == false) {
+			vx = 0;
+			dir *= -1;
+		}
+	}
+	
+	
+	public void update() {
+		if(dir == -1)
+			tx.setToTranslation(x-Camera.x+128, y-Camera.y);
+		else
+			tx.setToTranslation(x-Camera.x, y-Camera.y);
+		tx.scale(dir, 1);
+		boolean flag = false;
+		for(Block[] blockArray : LevelBuilder.level) {
+			for(Block block : blockArray) {
+				switch(collide(block)) {
+				case 1:
+					vx = 0;
+					dir *= -1;
+					break;
+	
+				case 2:
+					vx = 0;
+					dir *= -1;
+					break;
+	
+				case 3:
+					vy = 0;
+					grounded = true;
+					flag = true;
+				    break;
+	
+				case 4:
+					break;
+					
+				case 0:
+					if(flag == false) {
+						grounded = false;
+					}
+					break;
+				}
+			}
+		}
+		
+		checkGround();
+		
 		
 		
 		if(!grounded) {
-			vy -= 2;
-			y -= vy;
+			vy += 2;
+			vx = 0;
+		}
+		else 
+		{
+			vx = 8*dir;
+			moveTimer = 20;
 		}
 		
 		x += vx;
