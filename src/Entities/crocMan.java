d7aa2e25e8982f5d4cb238827056288145e19b87
auraw <auraw@DESKTOP-9SP0HG2>
2022-05-29 00:10:57 -0700

    added crocman back. It was accedentally deleted

---
+++ src/Entities/crocMan.java
@@ -0,0 +1,134 @@
+package Entities;
+
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.net.URL;
+
+import Blocks.Block;
+import Blocks.LevelBuilder;
+import General.Frame;
+
+public class crocMan extends Entity{
+
+	public int waitTimer, moveTimer;
+	public boolean frenzy;
+	
+	public crocMan(int x, int y, int w, int h, boolean visible, String path) {
+		super(x, y, w, h, visible, path);
+		frenzy = false;
+		Sprite = getImage("/imgs/Monsters/Crocman/crocman_Stand.gif");
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void detect() {
+		int mapX = (int) (x / 128), spelunkerX = (int) (Frame.Ana.x / 128);
+		int mapY = (int) (y / 128), spelunkerY = (int) (Frame.Ana.y / 128);
+		
+		if(mapY == spelunkerY && Math.abs(mapX - spelunkerX) <= 8) {
+			frenzy = true;
+			Sprite = getImage("/imgs/Monsters/Crocman/crocman_walk.gif");
+
+		}
+	}
+	
+	public void jump() {
+		if(grounded) {
+			vy = -45;
+		}
+	}
+	
+	public void update() {
+		if(dir == -1)
+			tx.setToTranslation(x-Camera.x+128, y-Camera.y);
+		else
+			tx.setToTranslation(x-Camera.x, y-Camera.y);
+		tx.scale(dir, 1);
+		boolean flag = false;
+		for(Block[] blockArray : LevelBuilder.level) {
+			for(Block block : blockArray) {
+				switch(collide(block)) {
+				case 1:
+					if(frenzy) {
+						vx *= -1;
+					}else {
+						vx = 0;
+					}
+					dir *= -1;
+					break;
+	
+				case 2:
+					if(frenzy) {
+						vx *= -1;
+					}else {
+						vx = 0;
+					}
+					dir *= -1;
+					break;
+	
+				case 3:
+					vy = 0;
+					grounded = true;
+					flag = true;
+				    break;
+	
+				case 4:
+					break;
+					
+				case 0:
+					if(flag == false) {
+						grounded = false;
+					}
+					break;
+				}
+			}
+		}
+		
+		detect();
+		
+		if(!frenzy) {
+			if(!grounded) {
+				vy += 2;
+			}else {
+				if(waitTimer > 0){
+					waitTimer--;
+					if(waitTimer <= 0){
+						vx = 8*dir;
+						moveTimer = 22;
+						Sprite = getImage("/imgs/Monsters/Crocman/crocman_walk.gif");
+					}
+				} else if(moveTimer > 0){
+					moveTimer--;
+				} else{
+					vx = 0;
+					waitTimer = 20;
+					Sprite = getImage("/imgs/Monsters/Crocman/crocman_stand.gif");
+
+				}
+			}
+		}else {
+			vx = dir * 10;
+			jump();
+		}
+		
+		x += vx;
+		y += vy;
+	}
+
+	public void paint(Graphics g) {
+		update();
+		Graphics2D g2 = (Graphics2D) g;
+		g2.drawImage(Sprite, (int) (x-Camera.x), (int) (y-Camera.y), dir * (int) w, (int) h, null);
+	}
+
+	protected Image getImage(String path) {
+
+		Image tempImage = null;
+		try {
+			URL imageURL = Entity.class.getResource(path);
+			tempImage    = Toolkit.getDefaultToolkit().getImage(imageURL);
+		} catch (Exception e) {e.printStackTrace();}
+		return tempImage;
+	}	
+}
